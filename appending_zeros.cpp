744ae4cb5c519f9014baa9baf7766ef90628ac93
Rutuja Gosavi <156993578+RutujaGosavi@users.noreply.github.com>
2024-08-26 15:10:10 +0530

    appending_zeros.cpp

--- appending_zeros.cpp
+++ appending_zeros.cpp
@@ -1,10 +1,5 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//Implement a problem of move all zeroes to end of array.
+//Given an array of random mumbers, Push all the zero's of a given array to the end of the array.
 
 #include<iostream>
 using namespace std;
